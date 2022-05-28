4418220001dfa45a9ef04e6a71a339ead3426818
ihfazhillah <mihfazhillah@gmail.com>
2022-05-28 18:24:47 +0700

    update child str

--- ksatria_muslim/children/models.py
+++ ksatria_muslim/children/models.py
@@ -10,3 +10,6 @@ class Child(TimeStampedModel):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey(User, on_delete=models.CASCADE)
     enable_read_to_me = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
