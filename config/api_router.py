f8f0b6c7787a1e1e25508c9ed0faf316b3e8c02a
ihfazhillah <mihfazhillah@gmail.com>
2022-06-01 21:12:14 +0700

    add book state viewset

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from ksatria_muslim.books.api.views import BookViewSet
+from ksatria_muslim.books.api.views import BookViewSet, BookStateViewSet
 from ksatria_muslim.children.api.views import ChildViewSet
 from ksatria_muslim.rewards.api.views import RewardHistoryViewSet
 from ksatria_muslim.users.api.views import UserViewSet
@@ -12,6 +12,7 @@ else:
     router = SimpleRouter()
 
 router.register("books", BookViewSet)
+router.register("books-state", BookStateViewSet)
 router.register("users", UserViewSet)
 router.register("child", ChildViewSet)
 router.register("reward-history", RewardHistoryViewSet)
