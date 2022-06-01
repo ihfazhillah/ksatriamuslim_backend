f356f963ebd28d3f6506f9b83ee5c7fcc5650ed9
ihfazhillah <mihfazhillah@gmail.com>
2022-06-01 11:16:22 +0700

    fix serializer error

--- ksatria_muslim/books/api/serializers.py
+++ ksatria_muslim/books/api/serializers.py
@@ -24,7 +24,7 @@ class PageSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    reference = BookReferenceSerializer()
+    # reference = BookReferenceSerializer()
     # page_set = PageSerializer(many=True)
 
     class Meta:
