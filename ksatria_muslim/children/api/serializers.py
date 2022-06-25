88015db6d1c8cc853c9e324e7b85f7c23af5060a
ihfazhillah <mihfazhillah@gmail.com>
2022-06-25 22:46:57 +0700

    Add set picture url

--- ksatria_muslim/children/api/serializers.py
+++ ksatria_muslim/children/api/serializers.py
@@ -2,14 +2,22 @@ from django.db import models
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from ksatria_muslim.children.models import Child
+from ksatria_muslim.children.models import Child, PhotoProfile
 from ksatria_muslim.rewards.models import REWARD_TYPES
 
 
+class PhotoProfileSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoProfile
+        fields = ["id", "photo"]
+
+
 class ChildSerializer(ModelSerializer):
+    picture = PhotoProfileSerializer(read_only=True)
+
     class Meta:
         model = Child
-        fields = ["id", "name", "enable_read_to_me", "points", "stars", "parent_id"]
+        fields = ["id", "name", "enable_read_to_me", "points", "stars", "parent_id", "picture"]
         read_only_fields = ["parent_id"]
 
     points = serializers.SerializerMethodField()
--- ksatria_muslim/children/api/views.py
+++ ksatria_muslim/children/api/views.py
@@ -1,8 +1,11 @@
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, ListModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from ksatria_muslim.children.api.serializers import ChildSerializer
-from ksatria_muslim.children.models import Child
+from ksatria_muslim.children.models import Child, PhotoProfile
 
 
 class ChildViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, CreateModelMixin, GenericViewSet):
@@ -11,3 +14,16 @@ class ChildViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, CreateM
 
     def get_queryset(self):
         return self.queryset.filter(parent=self.request.user)
+
+    @action(methods=["POST"], detail=True)
+    def set_picture(self, request, pk, *args, **kwargs):
+        obj = self.get_object()
+        picture_id = request.data.get("picture_id")
+        if picture_id:
+            return Response({"message": "Bad Request"}, status=400)
+
+        picture = get_object_or_404(PhotoProfile, pk=picture_id)
+        obj.picture = picture
+        obj.save()
+
+        return Response(ChildSerializer(obj).data)
