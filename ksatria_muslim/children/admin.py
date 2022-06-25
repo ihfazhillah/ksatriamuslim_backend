9547e8b20209b334edb7774ceab3cc043ed030ca
ihfazhillah <mihfazhillah@gmail.com>
2022-06-25 22:36:46 +0700

    add photo management in the cms

--- ksatria_muslim/children/admin.py
+++ ksatria_muslim/children/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-from ksatria_muslim.children.models import Child
+from ksatria_muslim.children.models import Child, PhotoProfile
 
 
 @admin.register(Child)
 class ChildAdmin(admin.ModelAdmin):
-    list_display = ("name", "parent_name")
+    list_display = ("name", "parent_name", "display_photo")
 
     @admin.display(description="Parent")
     def parent_name(self, obj):
         return obj.parent.email
+
+    @admin.display(description="Photo")
+    def display_photo(self, obj):
+        if not obj.picture:
+            return "no picture"
+        return format_html(f"<img src='{obj.picture.photo.url}' height='100' width='100'/>")
+
+
+@admin.register(PhotoProfile)
+class PhotoProfileAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "display_photo")
+
+    @admin.display(description="Photo")
+    def display_photo(self, obj):
+        return format_html(f"<img src='{obj.photo.url}' height='100' width='100'/>")
