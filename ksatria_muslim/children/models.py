d19aed6c96e847b8210a56d191f2fa9ccf1abf7c
ihfazhillah <mihfazhillah@gmail.com>
2022-06-25 22:29:28 +0700

    add photo profile model

--- ksatria_muslim/children/models.py
+++ ksatria_muslim/children/models.py
@@ -10,6 +10,16 @@ class Child(TimeStampedModel):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey(User, on_delete=models.CASCADE)
     enable_read_to_me = models.BooleanField(default=False)
+    picture = models.ForeignKey("PhotoProfile", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
+
+
+class PhotoProfile(TimeStampedModel):
+    photo = models.ImageField(upload_to="child_profiles/")
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
