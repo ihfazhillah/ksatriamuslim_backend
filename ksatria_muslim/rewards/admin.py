49ebf2f8397a979f3188e95e408aa020bb9d52f1
ihfazhillah <mihfazhillah@gmail.com>
2022-07-02 16:09:58 +0700

    add created

--- ksatria_muslim/rewards/admin.py
+++ ksatria_muslim/rewards/admin.py
@@ -5,7 +5,7 @@ from ksatria_muslim.rewards.models import RewardHistory
 
 @admin.register(RewardHistory)
 class RewardAdmin(admin.ModelAdmin):
-    list_display = ["child_name", "parent_name", "description", "reward_type", "count"]
+    list_display = ["child_name", "parent_name", "description", "reward_type", "count", "created"]
 
     @admin.display(description="Child Name")
     def child_name(self, obj):
