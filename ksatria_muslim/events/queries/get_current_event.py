7c73b19dafe2e2244ffd599a70c1cd402633a493
ihfazhillah <mihfazhillah@gmail.com>
2023-03-30 23:13:16 +0700

    Add simple get current event

--- ksatria_muslim/events/queries/get_current_event.py
+++ ksatria_muslim/events/queries/get_current_event.py
@@ -1,2 +1,27 @@
+import datetime
+
+from django.db import models
+from django.utils.timezone import localtime
+
+from ksatria_muslim.events.models import Event
+
+
+def serialize_event(event):
+    if not event:
+        return
+
+    return {"id": event.id}
+
+
 class GetCurrentEvent:
-    pass
+    def get_current_event(self):
+        now_local = localtime()
+        events = Event.objects.all().annotate(
+            diff_1=models.F("started_at") - models.Value(now_local)
+        ).filter(
+            diff_1__lte=datetime.timedelta(minutes=30)
+        )
+
+        event = events.first()
+
+        return {"event": serialize_event(event)}
--- ksatria_muslim/events/tests/queries/test_get_current_event.py
+++ ksatria_muslim/events/tests/queries/test_get_current_event.py
@@ -1,5 +1,11 @@
-from ksatria_muslim.events.queries.get_current_event import GetCurrentEvent
+import datetime
 
+import freezegun
+import pytest
+import pytz
+
+from ksatria_muslim.events.queries.get_current_event import GetCurrentEvent
+from ksatria_muslim.events.tests.factories import EventFactory
 
 """
 event sekarang adalah:
@@ -7,16 +13,34 @@ event sekarang adalah:
 2. event yang sedang berjalan
 3. event yang sudah berjalan 30 menit yang lalu
 """
+
+@pytest.mark.django_db
 class TestGetCurrentEvent:
     SUT = None
+    event_1 = None
+    event_2 = None
 
     def setup_method(self, method):
         self.SUT = GetCurrentEvent()
 
+        event_1_started_at = datetime.datetime(2023, 1, 10, 6, tzinfo=pytz.timezone("Asia/Jakarta"))
+        self.event_1 = EventFactory(
+            started_at=event_1_started_at
+        )
+
+        event_2_started_at = datetime.datetime(2023, 1, 10, 9, tzinfo=pytz.timezone("Asia/Jakarta"))
+        self.event_2 = EventFactory(
+            started_at=event_2_started_at
+        )
 
     def teardown_method(self, method):
-        pass
+        self.event_1.delete()
+        self.event_2.delete()
 
+    @freezegun.freeze_time("2023-01-10 05:00 WIB")
+    def test_no_current_event(self):
+        assert not self.SUT.get_current_event()["event"]
 
-    def _create_events(self):
-        pass
+    @freezegun.freeze_time("2023-01-10 05:30 WIB")
+    def test_before_30_minutes_of_first_event_returns_it(self):
+        assert self.SUT.get_current_event()["event"]["id"] == self.event_1.id
