4669d5f9c73e6965c460396f31196a8181d45790
ihfazhillah <mihfazhillah@gmail.com>
2023-04-01 20:59:07 +0700

    add upcoming events view

--- ksatria_muslim/events/di.py
+++ ksatria_muslim/events/di.py
@@ -1,10 +1,15 @@
 from ksatria_muslim.events.queries.get_current_event import GetCurrentEvent
+from ksatria_muslim.events.queries.get_upcoming_events import GetUpcomingEvents
 
 
 class EventCompositionRoot:
     @property
     def get_current_event_query(self):
         return GetCurrentEvent()
+
+    @property
+    def get_upcoming_events_query(self):
+        return GetUpcomingEvents()
 
 
 event_composition_root = EventCompositionRoot()
--- ksatria_muslim/events/urls.py
+++ ksatria_muslim/events/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from ksatria_muslim.events.views import get_current_event
+from ksatria_muslim.events.views import get_current_event, get_upcoming_events
 
 app_name = "events"
 
 
 urlpatterns = [
     path("current/", get_current_event, name="current"),
+    path("upcoming/", get_upcoming_events, name="upcoming"),
 ]
