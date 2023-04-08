f6a161d6ae8147b6b38fcb6ee2a28d3b02971ec2
ihfazhillah <mihfazhillah@gmail.com>
2023-04-08 22:00:34 +0700

    only authenticated users can get the url

--- ksatria_muslim/events/views.py
+++ ksatria_muslim/events/views.py
@@ -6,21 +6,18 @@ from ksatria_muslim.events.di import event_composition_root
 
 
 @api_view(["GET"])
-@permission_classes([AllowAny])
 def get_current_event(request):
     current_event = event_composition_root.get_current_event_query.get_current_event()
     return Response(current_event)
 
 
 @api_view(["GET"])
-@permission_classes([AllowAny])
 def get_upcoming_events(request):
     upcoming_events = event_composition_root.get_upcoming_events_query.get_upcoming_events(limit=5)
     return Response(upcoming_events)
 
 
 @api_view(["GET"])
-@permission_classes([AllowAny])
 def get_event_detail(request, event_id):
     upcoming_events = event_composition_root.get_event_detail_query.get_event(event_id)
     return Response(upcoming_events)
