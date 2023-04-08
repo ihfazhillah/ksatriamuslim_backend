fddc5c31973163fbdc8459f9af456dc13750593f
ihfazhillah <mihfazhillah@gmail.com>
2023-04-08 11:22:19 +0700

    Add ordering in the admin site

--- ksatria_muslim/events/admin.py
+++ ksatria_muslim/events/admin.py
@@ -9,6 +9,7 @@ admin.site.register(EventOrganizer, admin.ModelAdmin)
 class EventAdmin(admin.ModelAdmin):
     list_display = ("title", "presenter", "organizer", "started_at")
     fields = ("title", "thumbnail", "organizer", "presenter", "started_at", "youtube_link", "zoom_link")
+    ordering = ["-started_at"]
 
     def save_model(self, request: HttpRequest, obj, form, change) -> None:
         obj.submitter = request.user
