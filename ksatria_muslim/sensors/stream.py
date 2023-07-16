8564de4c043da8fdfc7d563470b79ae2304e21e0
ihfazhillah <mihfazhillah@gmail.com>
2023-07-16 18:50:27 +0700

    add stream and download module

---
+++ ksatria_muslim/sensors/stream.py
@@ -0,0 +1,90 @@
+import datetime
+import os.path
+import traceback
+from urllib.parse import urlparse
+
+import m3u8
+import requests
+
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+base_path = os.path.dirname(__file__)
+
+
+class M3U8Stream:
+    BASE_PATH = os.path.join(base_path, "streams")
+
+    def __init__(self):
+        # in seconds
+        self.timeout = 120
+        self.playlists = []
+        self.output_fname = ""
+
+        os.makedirs(self.BASE_PATH, exist_ok=True)
+
+    def download(self, url):
+        time = datetime.datetime.now()
+        tick = datetime.datetime.now()
+        while True:
+            if (tick - time).seconds >= self.timeout:
+                print("timeout")
+                break
+
+            remote_playlists = m3u8.load(url)
+            print(f"downloading playlist url, got : {len(remote_playlists.files)}")
+
+            for file_url in remote_playlists.files:
+
+                filename = urlparse(file_url).path.rsplit("/", maxsplit=1)[1]
+                print(filename)
+
+                if filename in self.playlists:
+                    print(f"file {filename} found, skip")
+                    continue
+
+                try:
+                    resp = requests.get(file_url)
+                    if resp.status_code == 404:
+                        print("file not found, skip me")
+                        continue
+
+                    with open(os.path.join(self.BASE_PATH, filename), "wb") as f:
+                        f.write(resp.content)
+
+                    self.playlists.append(filename)
+
+                except Exception as e:
+                    print(f"exception occurred: {e}")
+                    traceback.format_exc()
+                    continue
+
+            tick = datetime.datetime.now()
+
+    def concat_playlists(self):
+        def sorter_key(f):
+            key = int(f.split(".")[0].rsplit("_", 1)[1])
+            return int(key)
+
+        sorted_playlist = sorted(self.playlists, key=sorter_key)
+
+        clips = [VideoFileClip(os.path.join(self.BASE_PATH, f)) for f in sorted_playlist]
+        concatenated_clips = concatenate_videoclips(clips)
+
+        output_fname = os.path.join(self.BASE_PATH, "output.mp4")
+        concatenated_clips.write_videofile(output_fname)
+        self.output_fname = output_fname
+
+    def clean(self):
+        files = [
+            os.path.join(self.BASE_PATH, f) for f in self.playlists
+        ] + [self.output_fname]
+
+        for f in files:
+            os.remove(f)
+
+    def run(self, url):
+        self.download(url)
+        self.concat_playlists()
+        # self.clean()
+
