b4b3bae6fe9e826f897a74328c89be3a5c3ebc76
ihfazhillah <mihfazhillah@gmail.com>
2023-07-16 23:53:09 +0700

    update timeout to 25

--- ksatria_muslim/sensors/di.py
+++ ksatria_muslim/sensors/di.py
@@ -4,7 +4,7 @@ from ksatria_muslim.sensors.stream import M3U8Stream
 class SensorsCompositionRoot:
     @property
     def streamer(self):
-        return M3U8Stream()
+        return M3U8Stream(25)
 
 
 sensors_composition_root = SensorsCompositionRoot()
