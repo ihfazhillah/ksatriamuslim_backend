193921e790b2edb02d3b5da4f7f4bd18f32c1ee0
ihfazhillah <mihfazhillah@gmail.com>
2024-02-05 21:47:26 +0700

    fix route

--- ksatria_muslim/children_task/urls.py
+++ ksatria_muslim/children_task/urls.py
@@ -6,6 +6,6 @@ app_name = "children_task"
 
 urlpatterns = [
     path("children/", views.get_children, name="get-children"),
-    path("children/<id>/tasks/", views.get_task_list, name="get-task-list"),
+    path("children/<child_id>/tasks/", views.get_task_list, name="get-task-list"),
     path("mark-as-finished/", views.mark_as_finished, name="mark-as-finished"),
 ]
