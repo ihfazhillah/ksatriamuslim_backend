db624750f39da8903bdac621590e1628fb147b32
ihfazhillah <mihfazhillah@gmail.com>
2024-02-05 21:40:13 +0700

    fix instance

--- ksatria_muslim/children_task/selectors.py
+++ ksatria_muslim/children_task/selectors.py
@@ -6,7 +6,7 @@ from ksatria_muslim.children_task.models import Task, TaskHistory
 def get_children_tasks(child_id) -> list[TaskHistory]:
     tasks = Task.objects.filter(children__id=child_id)
     today_histories = [
-        TaskHistory.objects.get_or_create(created=timezone.now(), task=task, child_id=child_id)
+        TaskHistory.objects.get_or_create(created=timezone.now(), task_id=task.id, child_id=child_id)
         for task in tasks
     ]
-    return today_histories
+    return [instance for instance, created in today_histories]
