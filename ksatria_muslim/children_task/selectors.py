673ffc26b476ce0a268fbe996795b54cc37fe043
ihfazhillah <mihfazhillah@gmail.com>
2024-02-05 21:44:10 +0700

    fix date

--- ksatria_muslim/children_task/selectors.py
+++ ksatria_muslim/children_task/selectors.py
@@ -5,8 +5,9 @@ from ksatria_muslim.children_task.models import Task, TaskHistory
 
 def get_children_tasks(child_id) -> list[TaskHistory]:
     tasks = Task.objects.filter(children__id=child_id)
+    today = timezone.now().today()
     today_histories = [
-        TaskHistory.objects.get_or_create(created=timezone.now(), task_id=task.id, child_id=child_id)
+        TaskHistory.objects.get_or_create(created__date=today, task_id=task.id, child_id=child_id)
         for task in tasks
     ]
     return [instance for instance, created in today_histories]
