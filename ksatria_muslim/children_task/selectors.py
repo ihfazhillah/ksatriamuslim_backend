508e0ede410914d52a606d8e2855548af1814334
ihfazhillah <mihfazhillah@gmail.com>
2024-02-17 22:33:35 +0700

    sort task by updated

--- ksatria_muslim/children_task/selectors.py
+++ ksatria_muslim/children_task/selectors.py
@@ -10,4 +10,5 @@ def get_children_tasks(child_id) -> list[TaskHistory]:
         TaskHistory.objects.get_or_create(created__date=today, task_id=task.id, child_id=child_id)
         for task in tasks
     ]
+    today_histories = sorted(today_histories, key=lambda history: history.modified, reverse=True)
     return [instance for instance, created in today_histories]
