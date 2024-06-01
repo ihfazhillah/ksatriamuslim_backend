a383f80a634ba7dbd877be63a3945bb43e2ba54a
ihfazhillah <mihfazhillah@gmail.com>
2024-06-01 17:03:43 +0700

    add udzur and time

--- ksatria_muslim/children_task/views.py
+++ ksatria_muslim/children_task/views.py
@@ -56,7 +56,9 @@ def serialize_task(history: TaskHistory, request=None):
         "id": history.id,
         "title": history.task.title,
         "status": history.status,
-        "image": image_url
+        "image": image_url,
+        "udzur": history.udzur_reason,
+        "time": history.task.scheduled_at.strftime("%H:%M")
     }
 
 
