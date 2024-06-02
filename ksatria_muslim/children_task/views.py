9715bc7f4b7d5a91dbb4811c4281ff5a03b066fc
ihfazhillah <mihfazhillah@gmail.com>
2024-06-02 08:20:06 +0700

    fix typo

--- ksatria_muslim/children_task/views.py
+++ ksatria_muslim/children_task/views.py
@@ -70,7 +70,7 @@ def get_task_list(request, child_id):
         child_id=child_id,
         task__active=True,
         task__days__contains=[today.isoweekday()],
-        created_at__date=today
+        created__date=today
     ).order_by("task__scheduled_at", "task__title")
     return Response({"tasks": [serialize_task(task, request) for task in tasks]})
 
